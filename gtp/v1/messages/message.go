040bd24950b562574ba5181a2a421206aaac25a8
wuhao <wuhaotx@feinno.com>
2020-04-20 09:38:43 +0800

    add doc

--- gtp/v1/messages/message.go
+++ gtp/v1/messages/message.go
@@ -7,6 +7,34 @@ Package messages provides encoding/decoding feature of GTPv1 protocol.
 */
 package messages
 
+/**
+Refer by TS 29.281
+		Bits
+Octets		8	7	6	5	4	3	2	1
+1		Version	PT	(*)	E	S	PN
+2		Message Type
+3		Length (1st Octet)
+4		Length (2nd Octet)
+5		Tunnel Endpoint Identifier (1st Octet)
+6		Tunnel Endpoint Identifier (2nd Octet)
+7		Tunnel Endpoint Identifier (3rd Octet)
+8		Tunnel Endpoint Identifier (4th Octet)
+9		Sequence Number (1st Octet)1) 4)
+10		Sequence Number (2nd Octet)1) 4)
+11		N-PDU Number2) 4)
+12		Next Extension Header Type3) 4)
+
+Message Type value (Decimal)	Message	Reference	GTP-C	GTP-U	GTP'
+1	    Echo Request		X	X	x
+2	    Echo Response		X	X	x
+3-25	Reserved in 3GPP TS 32.295 [8] and 3GPP TS 29.060 [6]
+26	    Error Indication			X
+27-30	Reserved in 3GPP TS 29.060 [6]
+31	    Supported Extension Headers Notification		X	X
+32-253	Reserved in 3GPP TS 29.060 [6]
+254	    End Marker			X
+255	    G-PDU			X
+*/
 // Message Type definitions.
 const (
 	_ uint8 = iota
